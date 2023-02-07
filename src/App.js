2c506c72819698ed4466be2b1d0f701ef2f7699b
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-02-07 15:19:42 +0530

    edited done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Todolist from "./Component/TodoList";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Todolist />
     </div>
   );
 }
--- src/Component/TodoList.jsx
+++ src/Component/TodoList.jsx
@@ -3,27 +3,39 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const localItem = () => {
-  let listed = localStorage.getItem("lists");
+  let listed = localStorage.getItem("list");
   if (listed) {
-    return JSON.parse(listed);
+    return JSON.parse(localStorage.getItem("list"));
   } else {
     return [];
   }
 };
 
 export default function Todolist() {
-  const [text, setText] = useState(localItem());
-  const [task, setTask] = useState([]);
+  const [text, setText] = useState();
+  const [toggle, setToggle] = useState(true);
+  const [task, setTask] = useState(localItem());
+  const [edit, setEdit] = useState(null);
+
+  // edit the element
+  const edithandler = (id) => {
+    let newitem = task.find((el) => {
+      return el.id === id;
+    });
+    setToggle(false);
+    setText(newitem.name);
+    setEdit(id);
+  };
 
   // local storage
   useEffect(() => {
-    localStorage.setItem("lists", JSON.stringify(task));
+    localStorage.setItem("list", JSON.stringify(task));
   }, [task]);
 
   // delete
   const removehandler = (e) => {
-    const data = task.filter((value, index) => {
-      return index !== e;
+    const data = task.filter((value) => {
+      return value.id !== e;
     });
     setTask(data);
   };
@@ -35,8 +47,23 @@ export default function Todolist() {
   // saving input value into task array
   const submithandler = (e) => {
     e.preventDefault();
-    setTask([...task, text]);
-    setText("");
+    if (text && !toggle) {
+      setTask(
+        task.map((el) => {
+          if (el.id === edit) {
+            return { ...el, name: text };
+          }
+          return el;
+        })
+      );
+      setToggle(true);
+      setText("");
+      setEdit(null);
+    } else {
+      const inputdata = { id: new Date().getTime().toString(), name: text };
+      setTask([...task, inputdata]);
+      setText("");
+    }
   };
   return (
     <div>
@@ -50,16 +77,17 @@ export default function Todolist() {
           onChange={SaveText}
         />
 
-        <button type="submit">Add</button>
+        {toggle ? <button type="submit">Add</button> : <button>edit</button>}
       </form>
       <div>
-        {task.map((el, id) => {
+        {task.map((el) => {
           return (
             <div>
               <br />
-              <div key={id}>
-                {el}
-                <button onClick={() => removehandler(id)}>Sub</button>
+              <div key={el.id}>
+                {el.name}
+                <button onClick={() => removehandler(el.id)}>Sub</button>
+                <button onClick={() => edithandler(el.id)}>edit</button>
               </div>
             </div>
           );
